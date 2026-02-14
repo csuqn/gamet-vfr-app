2bf6a72f628618535778a66f585c908ab3a54fe6
csuqn <duartemanuelalmeida2@gmail.com>
2026-02-14 15:07:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -236,7 +236,7 @@ if st.button("🔍 Analisar GAMET") and gamet_text.strip():
     # MAPA (mais pequeno e centrado)
     st.subheader("🗺️ Mapa VFR")
 
-    fig, ax = plt.subplots(figsize=(5.5, 8))
+    fig, ax = plt.subplots(figsize=(6, 8.5))
 
     color_map = {"GO": "green", "MARGINAL": "orange", "NO-GO": "red"}
     zone_y = {"NORTE": (9,14), "CENTRO": (4,9), "SUL": (-4.5,4)}
